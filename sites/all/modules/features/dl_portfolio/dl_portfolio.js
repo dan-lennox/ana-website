a9f193c25f3a5675e69af75fe89386034a015de1
Daniel Lennox <lennox.dan@gmail.com>
2012-12-07 17:54:06 +1100

    Added dl_portfolio and ana_general features

---
+++ sites/all/modules/features/dl_portfolio/dl_portfolio.js
@@ -0,0 +1,52 @@
+// below is breaking default JS.
+
+(function ($) {
+	Drupal.behaviors.dl_portfolio = {
+		attach: function(context) {
+			//$('a[rel^=\'prettyPhoto\']').prettyPhoto();
+
+			//$(".fancybox").fancybox();
+
+      $(".jackbox[data-group]").jackBox("init", {
+
+        deepLinking: true,              // choose to use the deep-linking feature ("true" will enhance social sharing!) true/false
+        showInfoByDefault: false,       // show item info automatically when content loads, true/false
+        preloadGraphics: true,          // preload the jackbox graphics for a faster jackbox
+        fullscreenScalesContent: true,  // choose to always scale content up in fullscreen mode, true/false
+        //defaultShareImage: "1.jpg"          // if the social widget can't find an image to share from your lightbox content,
+                                        // it will use this image's url instead.  Only applicable to Pinterest.
+
+        //autoPlayVideo: false,           // video autoplay default, this can also be set per video in the data-attributes, true/false
+        //flashVideoFirst: false,         // choose which technology has first priority for video, HTML5 or Flash, true/false
+        //defaultVideoWidth: 960,         // the default video width, can also be set per video in the data-attributes
+        //defaultVideoHeight: 540,        // the default video width, can also be set per video in the data-attributes
+
+        useThumbs: true,                // choose to use thumbnails, true/false
+        thumbsStartHidden: false,       // choose to initially hide the thumbnail strip, true/false
+        thumbnailWidth: 100,             // the default video width, can also be set per video in the data-attributes
+        thumbnailHeight: 100,            // the default video height, can also be set per video in the data-attributes
+        //useThumbTooltips: true                 // choose to use small thumbnail tooltips, true/false
+
+      });
+      // thumbnailWidth: 75,
+
+			// $(".fancybox").fancybox({
+   //      openEffect  : 'none',
+   //      closeEffect : 'none',
+			//   helpers : {
+   //        media: { },
+   //        thumbs: {
+   //          width  : 100,
+   //          height : 100,
+   //          position : 'bottom',
+   //        }
+   //      }
+   //    });
+
+			// Shadowbox.init({
+			//         handleOversize: "drag",
+			//         modal: true
+			//       });
+		}
+	}
+})(jQuery);
